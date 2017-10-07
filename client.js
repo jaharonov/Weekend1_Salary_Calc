bfba8d48f6519373d9f61255a86b7474b7c09935
Jaffa Aharonov <jaharonov@gmail.com>
2017-10-07 17:07:01 -0500

    calculation working

--- client.js
+++ client.js
@@ -18,10 +18,18 @@ addToForm();
 
 function addToForm() {
 
-
-  var fields = $( "form" ).serializeArray();
+console.log($( "form" ).serializeArray());
+  //var fields = $( "form" ).serializeArray();
+  var annualSalary = $('[name="annualSalary"]').val();
+  var monthlyCost = Math.round(annualSalary / 12);
    $( "#fromForm" ).empty();
-   jQuery.each( fields, function( i, field ) {
-     $( "#fromForm" ).append($(children)[1].value + " " );
-   });
- }
+   //jQuery.each( fields, function( i, field ) {
+     $( "#fromForm" ).append('<th>' + $('[name="firstName"]').val() + '</th>');
+     $( "#fromForm" ).append('<th>' + $('[name="lastName"]').val() + '</th>');
+     $( "#fromForm" ).append('<th>' + $('[name="idNumber"]').val() + '</th>');
+     $( "#fromForm" ).append('<th>' + $('[name="jobTitle"]').val() + '</th>');
+     $( "#fromForm" ).append('<th>' + $('[name="annualSalary"]').val() + '</th>');
+     $( "#fromForm" ).append('<th>' + monthlyCost + '</th>');
+ //});
+//}
+}
