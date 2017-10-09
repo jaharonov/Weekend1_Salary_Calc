63eff7c68af13a780d91215c583a6384c04f50b2
Jaffa Aharonov <jaharonov@gmail.com>
2017-10-08 21:44:10 -0500

    cleaned up

--- client.js
+++ client.js
@@ -1,43 +1,25 @@
 console.log('script sourced!');
 $(document).ready(readyNow);
 
-
 function readyNow() {
-console.log('ready!');
-$( "form" ).on( "submit", function( event ) {
-  event.preventDefault();
+  console.log('ready!');
+  $( "form" ).on( "submit", function( event ) {
+    event.preventDefault();
+    addToForm();
+    $("form").trigger("reset");
+  });
+}
+function addToForm() {
+  console.log($( "form" ).serializeArray());
+  var monthlyCost = Math.round($('[name="annualSalary"]').val() / 12);
+  $("tbody").append('<tr>');
+  $( "tbody" ).append('<td>' + $('[name="firstName"]').val() + '</td>');
+  $( "tbody" ).append('<td>' + $('[name="lastName"]').val() + '</td>');
+  $( "tbody" ).append('<td>' + $('[name="idNumber"]').val() + '</td>');
+  $( "tbody" ).append('<td>' + $('[name="jobTitle"]').val() + '</td>');
+  $( "tbody" ).append('<td>' + $('[name="annualSalary"]').val() + '</td>');
+  $( "#total" ).append('<br>' + monthlyCost);
 
-addToForm();
-
-$("form").trigger("reset");
-
-
-
-
-/*$( "form" ).on( "submit", function( event ) {
-  event.preventDefault();
-  console.log( $("form").serializeArray() );
-});*/
-
-
-});
 
 
 }
-
-function addToForm() {
-console.log($( "form" ).serializeArray());
-  //var fields = $( "form" ).serializeArray();
-var monthlyCost = Math.round($('[name="annualSalary"]').val() / 12);
-   //jQuery.each( fields, function( i, field ) {
-     $("tbody").append('<tr>');
-     $( "tbody" ).append('<td>' + $('[name="firstName"]').val() + '</td>');
-     $( "tbody" ).append('<td>' + $('[name="lastName"]').val() + '</td>');
-     $( "tbody" ).append('<td>' + $('[name="idNumber"]').val() + '</td>');
-     $( "tbody" ).append('<td>' + $('[name="jobTitle"]').val() + '</td>');
-     $( "tbody" ).append('<td>' + $('[name="annualSalary"]').val() + '</td>');
-     $( "#total" ).append(monthlyCost);
-
-
-
-     }
