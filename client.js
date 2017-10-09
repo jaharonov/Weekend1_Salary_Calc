5a0677d8b2b5c6fa01164ff5d694ac118700cfc2
Jaffa Aharonov <jaharonov@gmail.com>
2017-10-09 13:40:43 -0500

    fixed monthly totals

--- client.js
+++ client.js
@@ -1,25 +1,37 @@
 console.log('script sourced!');
 $(document).ready(readyNow);
-
+var averageSalary = 0;
 function readyNow() {
   console.log('ready!');
-  $( "form" ).on( "submit", function( event ) {
+  $( 'form' ).on( 'submit', function( event ) {
     event.preventDefault();
     addToForm();
-    $("form").trigger("reset");
+    averageSalary += parseInt($('[name="annualSalary"]').val());
+    console.log('averageSalary', $('[name="annualSalary"]').val());
+    $('#averageMonthly').text('$' + calculateAverageSalary());
+    $('form').trigger('reset');
   });
 }
-function addToForm() {
-  console.log($( "form" ).serializeArray());
-  var monthlyCost = Math.round($('[name="annualSalary"]').val() / 12);
-  $("tbody").append('<tr>');
-  $( "tbody" ).append('<td>' + $('[name="firstName"]').val() + '</td>');
-  $( "tbody" ).append('<td>' + $('[name="lastName"]').val() + '</td>');
-  $( "tbody" ).append('<td>' + $('[name="idNumber"]').val() + '</td>');
-  $( "tbody" ).append('<td>' + $('[name="jobTitle"]').val() + '</td>');
-  $( "tbody" ).append('<td>' + $('[name="annualSalary"]').val() + '</td>');
-  $( "#total" ).append('<br>' + monthlyCost);
 
+
+
+function addToForm() {
+  console.log($( 'form').serializeArray());
+  //var monthlyCost = Math.round($('[name="annualSalary"]').val() / 12);
+  $('tbody').append('<tr>');
+  $( 'tbody' ).append('<td>' + $('[name="firstName"]').val() + '</td>');
+  $( 'tbody').append('<td>' + $('[name="lastName"]').val() + '</td>');
+  $( 'tbody' ).append('<td>' + $('[name="idNumber"]').val() + '</td>');
+  $( 'tbody' ).append('<td>' + $('[name="jobTitle"]').val() + '</td>');
+  $( 'tbody' ).append('<td>' + $('[name="annualSalary"]').val() + '</td>');
 
 
 }
+
+
+
+function calculateAverageSalary() {
+   // Calculate the monthly average
+   var averageMonthly= Math.round(averageSalary / 12);
+   return averageMonthly;
+ }
